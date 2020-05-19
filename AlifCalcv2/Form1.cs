fef99c0390659b3443ac8f282b9697e1309d46bb
GhostX <egamovkhurshed29@gmail.com>
2020-05-19 23:55:16 +0500

    Added some techniques

--- AlifCalcv2/Form1.cs
+++ AlifCalcv2/Form1.cs
@@ -16,31 +16,91 @@ namespace AlifCalcv2
         {
             InitializeComponent();
         }
-        
+        static string number = "";
+        static double x = 0;
+        static double y = 0;
+        static char function = ' ';
 
         private void Num_Click(object sender, EventArgs e)
         {
-            
+            Button btn = (Button)sender;
+            number += btn.Text;
+            PanelTBox.Text = number;
         }
 
         private void BackspaceBtn_Click(object sender, EventArgs e)
         {
-            
+            char[] nums = number.ToCharArray();
+            number = "";
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                number += nums[i];
+            }
+            PanelTBox.Text = number;
         }
 
         private void ClearElementBtn_Click(object sender, EventArgs e)
         {
-            
+            number = "";
+            y = 0;
+            PanelTBox.Text = "0";
         }
 
         private void ClearBtn_Click(object sender, EventArgs e)
         {
-            
+            number = "";
+            y = 0;
+            x = 0;
+            PanelTBox.Text = "0";
+            FunctionLbl.Text = "";
         }
 
         private void Function_Click(object sender, EventArgs e)
         {
-            
+            Button btn = (Button)sender;
+            function = char.Parse(btn.Text);
+            FunctionLbl.Text = function.ToString();
+            if (x == 0)
+            {
+                x = double.Parse(number);
+                number = "";
+            }
+            else
+            {
+                y = double.Parse(number);
+                number = "";
+                technique();
+            }
+        }
+
+        private void technique()
+        {
+            switch (function)
+            {
+                case '+':
+                    {
+                        x += y;
+                    }
+                    break;
+                case '/':
+                    {
+                        x /= y;
+                    }
+                    break;
+                case '*':
+                    {
+                        x *= y;
+                    }
+                    break;
+                case '-':
+                    {
+                        x -= y;
+                    }
+                    break;
+            }
+            y = 0;
+            function = ' ';
+            PanelTBox.Text = x.ToString();
         }
     }
 }
